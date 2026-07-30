ab9c207e42e480ccfd6eba8ce0134adb678aa5f2
Kim Nguyen <design@kimisgold.com>
2026-07-30 10:45:54 -0400

    Use sourcemaps, update dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,17 @@ gulp.task('css', function() {
     var sass = require('gulp-sass')(require('sass'));
     var postcss = require('gulp-postcss');
     var autoprefixer = require('autoprefixer');
+    var sourcemaps = require('gulp-sourcemaps');
 
     return gulp.src('./css/sass/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({
             includePaths: ['./node_modules/susy/sass']
         }).on('error', sass.logError))
         .pipe(postcss([
             autoprefixer()
         ]))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./css'));
 });
 
