032e9b8c356ddadbb7d9a8f9c58566bf569dc272
Kim Nguyen <design@kimisgold.com>
2026-08-20 12:56:18 -0400

    Add h1 to homepage.
    (omeka/Omeka#1059)

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php echo head(array('bodyid'=>'home')); ?>
 
+<h1 class="sr-only"><?php echo __('Home'); ?></h1>
+
 <?php if (get_theme_option('Homepage Text')): ?>
 <p class="intro"><?php echo get_theme_option('Homepage Text'); ?></p>
 <?php endif; ?>
